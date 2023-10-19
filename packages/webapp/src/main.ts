33494d7853b0a4ba5135d38e3a6e7cdaac1e8b38
drakejin(jin.search) <dydwls121200@gmail.com>
2023-10-20 01:31:31 +0900

    deploy lambda

--- packages/webapp/src/main.ts
+++ packages/webapp/src/main.ts
@@ -1,7 +1,25 @@
+import serverlessFastify, { CallbackHandler } from '@fastify/aws-lambda';
+import { Callback, Context, Handler } from 'aws-lambda';
 import { createServer } from './server';
+
+let app;
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  app = app ?? (await createServer());
+  const instance = app.getHttpAdapter().getInstance();
+  const lambda: CallbackHandler = serverlessFastify(instance);
+  console.log(event);
+  return lambda(event, context, callback);
+};
 
 async function start() {
   const app = await createServer();
   app.listen(process.env.PORT ?? 3000);
 }
-start();
+if (process.env.NEST_ENV !== 'lambda') {
+  start();
+}
